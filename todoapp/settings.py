2c69f530e559252732a0e5b86bea71b947b46f1d
Justblue0312 <71886798+Justblue0312@users.noreply.github.com>
2021-10-25 20:58:14 +0700

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoapp',
+        'USER': 'postgres',
+        'PASSWORD': '29091999trao*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
